49b0384609af8d32f28a165dabcf80224adafc24
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-09 11:39:49 -0500

    testing class

--- src/com/ngarambe/practice/Main.java
+++ src/com/ngarambe/practice/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	Student st = new Student(12062,"Ngarambe John",35);
         System.out.println(st.getId()+ " "+ st.getName() + "" + st.getAge());
 	st.studying();
+	//;;;;;;;;;;;;;;;;;
     }
 }
