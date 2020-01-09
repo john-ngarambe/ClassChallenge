abfbbcfd8649387e0dc96f342e5fcf187844c9bb
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-09 12:19:59 -0500

    modification on the project

--- src/com/ngarambe/practice/Main.java
+++ src/com/ngarambe/practice/Main.java
@@ -1,11 +1,17 @@
 package com.ngarambe.practice;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	Student st = new Student(12062,"Ngarambe John",35);
+		System.out.println("Student details:");
         System.out.println(st.getId()+ " "+ st.getName() + "" + st.getAge());
 	st.studying();
-	//;;;;;;;;;;;;;;;;;
-    }
+	Student stc = new Course(st.getId(),"Ngarambe John",35,"Java08  ","Core Java ",10);
+	stc.studying();
+
+
+}
 }
